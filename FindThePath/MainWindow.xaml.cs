412b2d159a6e2633365ff4c76346dcaa15af1693
Artur <shalaev-artur@yandex.ru>
2017-02-22 03:39:11 +0300

    MainWindow: DataGrid, Main button, Styles, Point2D
    == DataGrid
    +Constants: AREA_WIDTH = 50, AREA_HEIGHT = 30
    +DataGrid
    +Binding to DataTable (AREA_WIDTH x AREA_HEIGHT)
    
    == Main button
    +Button 'Calculate'
    
    == Styles
    +Window StaticResources
    
    == Point2D
    +Point2D struct (X, Y: INT coords) with ToString() overriding

--- FindThePath/MainWindow.xaml.cs
+++ FindThePath/MainWindow.xaml.cs
@@ -12,17 +12,93 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace FindThePath
 {
+    public struct Point2D
+    {
+        public int X, Y;
+
+        public Point2D(int x = 0, int y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1}", X, Y);
+        }
+    }
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int AREA_WIDTH = 50, AREA_HEIGHT = 30;
+        private static DataTable _data = new DataTable();
+        private Point2D A, B;
+
+        private void _grid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            string newText = (e.EditingElement as TextBox).Text;
+            switch (newText)
+            {
+                case "a":
+                case "A":
+                    if ((A.X != -1)&&(A.Y != -1))
+                    {
+                        Console.WriteLine("Old point 'A': {0}", A.ToString());
+                        _data.Rows[A.X][A.Y] = "";
+                        Console.WriteLine("Deleted old 'A' point from grid");
+                    }
+                    A.X = e.Row.GetIndex();
+                    A.Y = e.Column.DisplayIndex;
+                    Console.WriteLine("New point A: {0}", A.ToString());
+                    break;
+                case "b":
+                case "B":
+                    if ((B.X != -1) && (B.Y != -1))
+                    {
+                        Console.WriteLine("Old point 'B': {0}", A.ToString());
+                        _data.Rows[B.X][B.Y] = "";
+                        Console.WriteLine("Deleted old 'B' point from grid");
+                    }
+                    B.X = e.Row.GetIndex();
+                    B.Y = e.Column.DisplayIndex;
+                    Console.WriteLine("New point B: {0}", B.ToString());
+                    break;
+                default:
+                    Console.WriteLine("New point wasn't A or B: {0}. Deleted input value", newText);
+                    (e.EditingElement as TextBox).Text = "";
+                    break;
+            }
+            _grid.UpdateLayout();
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            A.X = A.Y = B.X = B.Y = -1;
+
+            for (int i = 0; i < AREA_WIDTH; i++)
+            {
+                _data.Columns.Add(i.ToString());
+            }
+
+            for (int i = 0; i < AREA_HEIGHT; i++)
+            {
+                _data.Rows.Add(new string[AREA_WIDTH]);
+            }
+            
+            _grid.DataContext = _data.DefaultView;
+            _grid.HeadersVisibility = DataGridHeadersVisibility.None;
+            _grid.UpdateLayout();
+
+            //_data.Rows[0][5] = 5;
         }
     }
 }
