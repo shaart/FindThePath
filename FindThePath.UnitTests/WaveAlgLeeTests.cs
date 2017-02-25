47e0b2849ddf9e47481c62e682412a09b5793d7d
Artur <shalaev-artur@yandex.ru>
2017-02-25 13:22:11 +0300

    Added UnitTests project

---
+++ FindThePath.UnitTests/WaveAlgLeeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindThePath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindThePath.Tests
+{
+    [TestClass()]
+    public class WaveAlgLeeTests
+    {
+        [TestMethod()]
+        public void GetFromStringTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
