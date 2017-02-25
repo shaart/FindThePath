10d1cf9dabec445a80251f3cc6c3ad0be1c375f6
Artur <shalaev-artur@yandex.ru>
2017-02-25 13:11:28 +0300

    Added WaveObject
    Struct contains:
    @ int? Value (can be 'null')
    @ ObjectType (StartPoint, EndPoint, Block, None)
    
    Has overrided ToString() method

---
+++ FindThePath/WaveObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindThePath
+{
+    public struct WaveObject
+    {
+        public int? Value;
+        public ObjectType Obj;
+
+        public WaveObject(int? value = null, ObjectType obj = ObjectType.None)
+        {
+            Value = value;
+            Obj = obj;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Obj.ToString(), Value);
+        }
+    }
+}
