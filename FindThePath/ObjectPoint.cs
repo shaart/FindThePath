dd62dd62c3dc85149baa292bb4c50a653f85423c
Artur <shalaev-artur@yandex.ru>
2017-02-25 13:20:36 +0300

    Added ObjectPoint class, enum ObjectType
    == ObjectPoint
    +int X,Y - coords
    +ObjectType Obj - object at point
    +Constructor
    +Overrided ToString() method
    
    == enum ObjectType
    +None
    +StartPoint
    +EndPoint
    +Block

---
+++ FindThePath/ObjectPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindThePath
+{
+    public enum ObjectType { None, StartPoint, EndPoint, Block }
+
+    public struct ObjectPoint
+    {
+        public int X, Y;
+        public ObjectType Obj;
+
+        public ObjectPoint(int x = 0, int y = 0, ObjectType obj = ObjectType.None)
+        {
+            X = x;
+            Y = y;
+            Obj = obj;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1} (Type: {2})", X, Y, Obj.ToString());
+        }
+    }
+}
